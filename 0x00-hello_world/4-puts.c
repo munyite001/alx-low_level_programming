28b082de80c8a8b7814df269c947a99482a24f5c
munyite001 <emunyite@gmail.com>
2022-09-08 09:18:05 -0700

    Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+
+
+/**
+ * main - Entry point
+ * outputs - "Programming is like building a multilingual puzzle
+ * Returns 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
