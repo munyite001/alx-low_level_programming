c68785ae792afa7bf164a909402a5e78510631cb
munyite001 <emunyite@gmail.com>
2022-09-08 09:29:56 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,9 +6,9 @@
  *  * main - Entry point
  *   * outputs - "with proper grammar, but the outcome is a piece of art,"
  *    * Return:  0 (Success)
- *     */
+ */
 int main(void)
 {
-	        printf("with proper grammar, but the outcome is a piece of art,\n");
-		return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
