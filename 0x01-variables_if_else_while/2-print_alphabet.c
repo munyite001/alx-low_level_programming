bc5a120725b9a4649f11876357e0811675959426
munyite001 <emunyite@gmail.com>
2022-09-08 22:41:53 -0700

    Alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+
+/**
+ * Entry Point: Main
+ * Operation: Print lowercase letters of the alphabet
+ * Return: 0 Success
+*/
+
+int main(void)
+{
+	char n = 'a';
+
+	for (int i = 0; i < 26; i++)
+	{
+		putchar(n);
+		n+=1;
+	}
+	putchar('\n');
+
+	return 0;
+}
