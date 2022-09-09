cf3f463579a7fd1ad2a19b84fd1855aa0acb8d6f
munyite001 <emunyite@gmail.com>
2022-09-09 08:00:17 -0700

    Last Digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -28,7 +28,7 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, lastdigit);
 	}
-	else if(lastdigit > 0 && lastdigit < 6)
+	else if(lastdigit != 0 && lastdigit < 6)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdigit);
 	}
