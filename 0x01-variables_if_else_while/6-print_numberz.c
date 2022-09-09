084c74315b6c072fea40b68dc05aaca64a4d0cc8
munyite001 <emunyite@gmail.com>
2022-09-09 11:03:18 -0700

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,7 +2,7 @@
 
 /**
  *  * main - Print numbers
- * 
+ *
  * Description: print all single digit number from 0 t0 9
  * Return: 0 success
  */
@@ -11,12 +11,13 @@ int main(void)
 {
 	int i;
 	int j = '0';
+
 	for (i = 0; i < 10; i++)
 	{
 		putchar(j);
-		j+=1;
+		j += 1;
 	}
 	putchar('\n');
 
-	return 0;
+	return (0);
 }
