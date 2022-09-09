90dee551f7ccfb595abf122611af40168180068c
munyite001 <emunyite@gmail.com>
2022-09-09 09:49:28 -0700

    Reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+
+/**
+ * Main: print alphabets in reverse
+ * Description: Print lowercase letters of the alphabet in reverse
+ * Return: 0 Success
+*/
+
+int main(void)
+{
+	char n = 'z';
+	int i;
+	for (i = 0; i < 26; i++)
+	{
+		putchar(n);
+		n -= 1;
+	}
+	putchar('\n');
+
+	return 0;
+}
