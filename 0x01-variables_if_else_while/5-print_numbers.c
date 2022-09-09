4fd694e17713e8275daeaf0bf2fecab81d66de9b
munyite001 <emunyite@gmail.com>
2022-09-09 09:33:08 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *  * main - Print numbers
+ * 
+ * Description: print all single digit number from 0 t0 9
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+
+	return 0;
+}
