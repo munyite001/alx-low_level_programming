161ce2903edb8b8d6b256165da53f295758976f2
munyite001 <emunyite@gmail.com>
2022-09-13 07:00:46 -0700

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -24,14 +24,14 @@ void times_table(void)
 			}
 			else
 			{
-				if (result < 10)
+				if (i * j+1 > 9)
 				{
-					printf("%d,  ", result);
+					printf("%d, ", result);
 				}
 
 				else
 				{
-					printf("%d, ", result);
+					printf("%d,  ", result);
 				}
 			}
 			j += 1;
