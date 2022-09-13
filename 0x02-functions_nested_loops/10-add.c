de82159488507d02d3605ed7b3d9d22d4e23ea65
munyite001 <emunyite@gmail.com>
2022-09-13 05:06:15 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - adds 2 integers
+ * Description: adds 2 integers x and y
+ * and returns the result
+ * @x: first integer to be added
+ * @y: second integer to be added
+ * Return: the result
+ */
+
+int add(int x, int y)
+{
+	int z = x + y;
+
+	return (z);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -18,3 +18,4 @@ int print_sign(int n);
 int _abs(int n);
 int print_last_digit(int n);
 
+int add(int x, int y);
