f470a8ffa218db9ad3489906c9719cf96f0d9b81
munyite001 <emunyite@gmail.com>
2022-09-13 04:56:17 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - returns value of last digit
+ *
+ * Description: returns value of last digit of a number n
+ *@n: the number
+ * Return: last digit of a number
+ */
+
+int print_last_digit(int n)
+{
+	int i = n % 10;
+
+	return (i);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -16,4 +16,5 @@ int _islower(char c);
 int _isalpha(char c);
 int print_sign(int n);
 int _abs(int n);
+int print_last_digit(int n);
 
