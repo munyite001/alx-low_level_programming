f35cb10f344e331d16abc3c0478e4c70ff1bbfb5
munyite001 <emunyite@gmail.com>
2022-09-13 01:38:31 -0700

    Print '_putchar'

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints "_putchar" to the screen
+ * Description: a simple program that prints 
+ * the text "_putchar" to the standard output
+ * followed by a new line
+ *
+ * return: (0) success
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
