e3e484009668bdc9a9fcd67631233661ad44f07f
munyite001 <emunyite@gmail.com>
2022-09-13 01:40:14 -0700

    Print '_putchar'

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,11 +2,11 @@
 
 /**
  * main - prints "_putchar" to the screen
- * Description: a simple program that prints 
+ * Description: a simple program that prints
  * the text "_putchar" to the standard output
  * followed by a new line
  *
- * return: (0) success
+ * Return: (0) success
  */
 
 int main(void)
