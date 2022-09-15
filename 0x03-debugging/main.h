6cb08fda6b89b0ac70fcd305c645161cdb1c1a37
munyite001 <emunyite@gmail.com>
2022-09-14 22:14:32 -0700

    Positive or negative test

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,23 +1,8 @@
-#include <unistd.h>
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
+#include <stdio.h>
 
-/* Function Prototypes*/
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(char c);
-int _isalpha(char c);
-int print_sign(int n);
-int _abs(int n);
-int print_last_digit(int n);
-void jack_bauer(void);
-void times_table(void);
-int add(int x, int y);
-void print_to_98(int n);
+void positive_or_negative(int i);
+
+#endif /* MAIN_H */
