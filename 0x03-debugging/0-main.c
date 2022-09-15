99b91cf576cff19428a2ea2ed42bde345015b5ed
munyite001 <emunyite@gmail.com>
2022-09-14 22:04:06 -0700

    First commit

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints if integer is positive or negative
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i = 98;
+
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,23 @@
+#include <unistd.h>
+
+/**
+ * putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+/* Function Prototypes*/
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(char c);
+int _isalpha(char c);
+int print_sign(int n);
+int _abs(int n);
+int print_last_digit(int n);
+void jack_bauer(void);
+void times_table(void);
+int add(int x, int y);
+void print_to_98(int n);
