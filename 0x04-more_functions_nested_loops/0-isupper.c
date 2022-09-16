1a2fe4e2c09788ff919553996604de9bd58d799b
munyite001 <emunyite@gmail.com>
2022-09-16 06:12:38 -0700

    IsUpper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -8,18 +8,10 @@
 
 int _isupper(char c)
 {
-	char upper[] = {'A','B','C','D','E','F','G','H','I','J','K',
-	'L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
-	int i;
-
-	for (i = 0; i < 26; i++)
+	if (c >= 'A' && c <= 'Z')
 	{
-		if (c == upper[i]);
-		{
-			return (1);
-		}
+		return (1);
 	}
-
 	return (0);
 }
 
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,13 +1,23 @@
-#include <unistd.h>
+#ifndef MAIN_H
+#define MAIN_H
 
 /**
- * putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
+ * *main - header file for protoypes
+ * *void protoypes (void)
+ * *int protoypes (int)
+ * */
 
-/* Function Prototypes*/
-int _putchar(char c);
-int _isupper(char c);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+int _putchar(char);
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
+
+#endif /* MAIN_H */
