94c7a04820d8e05104e8a0340406cbabf67fe7f3
munyite001 <emunyite@gmail.com>
2022-09-19 07:49:58 -0700

    String Length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,20 +2,19 @@
 
 /**
  * _strlen - returns the length of a string
- * s: a pointer to a string
+ * @s: a pointer to a string
  * Return: the length of the string
  */
 
 int _strlen(char *s)
 {
-	int i,counter = 0;
+	int i = 0;
 
 	while (*(s + i) != '\0')
 	{
 		i += 1;
-		counter += 1;
 
 	}
 
-	return (counter);
+	return (i);
 }
