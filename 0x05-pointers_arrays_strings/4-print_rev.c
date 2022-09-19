432751e0417a3ebbd6b88628165df79dd7fb48ab
munyite001 <emunyite@gmail.com>
2022-09-19 08:10:00 -0700

    reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: a pointer to a string
+ * Return: nothing
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		i += 1;
+	}
+
+	int j = i;
+
+	while (*(s + j) != *(s + 0))
+	{
+		_putchar(*(s + j));
+		j -= 1;
+	}
+	_putchar('\n');
+}
