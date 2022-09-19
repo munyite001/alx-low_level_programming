5829406178caec8200744b5112758e7d5db0ba63
munyite001 <emunyite@gmail.com>
2022-09-18 21:57:47 -0700

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * s: a pointer to a string
+ * Return: the length of the string
+ */
+
+int _strlen(char *s)
+{
+	char str[] = *s;
+	int i,counter = 0;
+
+	while (str[i] != '\0')
+	{
+		i += 1;
+		counter += 1;
+
+		return (counter);
+	}
+}
