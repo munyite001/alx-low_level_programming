c9eea8aab2ad725f5725a34d6bcb63290e262869
munyite001 <emunyite@gmail.com>
2022-09-19 07:55:47 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string folowed by a new line
+ * @str: a pointer to a string
+ * Return: nothing
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		_putchar('%c',*(str + i));
+		i += 1;
+	}
+	_putchar('\n');
+}
