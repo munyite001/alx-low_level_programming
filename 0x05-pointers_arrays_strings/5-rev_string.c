8810a69bbd0d76792dd0e999041dca72af20bc98
munyite001 <emunyite@gmail.com>
2022-09-19 08:51:08 -0700

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include "string.h"
+/**
+ * rev_string - reverses a string
+ * @c: pointer to a string
+ * Return: nothing
+ */
+
+void rev_string(char *s)
+{
+	char str[];
+	int len;
+	int i = 0;
+
+	strcpy(str, s);
+	len = strlen(str);
+
+	while(len--)
+	{
+		*(s + i) = *(str + len);
+		i += 1;
+	}
+
+}
