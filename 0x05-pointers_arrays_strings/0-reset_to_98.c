14d19281c612a89f55fdf7c3653e62fd2afb0a00
munyite001 <emunyite@gmail.com>
2022-09-18 21:36:20 -0700

    Pointers

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,12 +3,11 @@
 /**
  * reset_to_98 - sets a pointer to 98
  * @n: the pointer to be updated
- * Return: the pointer with updated value
+ * Return: Nothing
  */
 
 void reset_to_98(int *n)
 {
 	*n = 98;
 
-	return (n);
 }
