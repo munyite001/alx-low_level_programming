dafa22d5761f89a0ef21862a94d4e9c95d70ffbb
munyite001 <emunyite@gmail.com>
2022-09-19 09:37:51 -0700

    Half-string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string;
+ * @str: the pointer to the string
+ * Return: nothing
+ */
+
+void puts_half(char *str)
+{
+	int i = 0;
+	int n;
+
+	while (*(str + i) != '\0')
+	{
+		i += 1;
+	}
+
+	if (i % 2 != 0)
+	{
+		n = (i - 1) / 2;
+	}
+	else
+	{
+		n = i / 2;
+	}
+
+	while (*(str + n) != '\0')
+	{
+		_putchar(*(str + n));
+		n += 1;
+	}
+
+}
