91fc4d0ee3ef7d25699c62e984e4e7c82384cf33
munyite001 <emunyite@gmail.com>
2022-09-19 09:13:37 -0700

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts2 - print every other character of a string
+ * @str: pointer to a string
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+	int len = strlen(len);
+
+	while (i <= len)
+	{
+		_putchar(*(str + i));
+		i += 2;
+	}
+}
