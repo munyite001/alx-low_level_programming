a50a9fb1308b7c0f231513304187e9d6cadbe96f
munyite001 <emunyite@gmail.com>
2022-09-26 08:42:05 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+*_strstr - Locatesasubstring.
+*@haystack: Thestringtobesearched.
+*@needle: Thesubstringtobelocated.
+*
+*Return: Ifthesubstringislocated-apointertothebeginning
+* of the located substring.
+*If the sub string is not located-NULL.
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	int index;
+	
+	if (*needle == 0)
+        {
+		return(haystack);
+        }
+        
+        while ( *haystack)
+        {
+                index=0;
+
+                if(haystack[index] == needle[index])
+                {
+                        do
+                        {
+                                if(needle[index+1]=='\0')
+                                {
+                                        return(haystack);
+                                }
+                        index++;
+
+                        }
+                        while (haystack[index] == needle[index]);
+                }
+
+                haystack++;
+        }
+
+        return('\0');
+}
+
