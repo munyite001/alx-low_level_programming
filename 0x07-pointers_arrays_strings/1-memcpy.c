8ec0107e110073df06b664e916c32a496dea3c80
munyite001 <emunyite@gmail.com>
2022-09-26 08:03:00 -0700

    Mem Copy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,9 +11,9 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; n > 0; i++,n--)
+	for (i = 0; n > 0; i++, n--)
 	{
-		dest[i]=src[i];
+		dest[i] = src[i];
 	}
 
 	return (dest);
