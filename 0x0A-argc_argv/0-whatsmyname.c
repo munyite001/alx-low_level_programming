3573946148f5409ea3974cfbd49210d3e7674af5
munyite001 <emunyite@gmail.com>
2022-10-04 08:06:24 -0700

    what's my name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,8 +8,8 @@
  * Return: Success 0
  */
 
-int main(int argc __attribute ((unused)), char *argv[])
+int main(int argc __attribute((unused)), char *argv[])
 {
-	printf("%s\n", argv[0]);
+	printf("%s\n", *argv);
 	return (0);
 }
