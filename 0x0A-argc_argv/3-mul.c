c78a994ac9e061bda74d2dc0a015b1123ed09cbf
munyite001 <emunyite@gmail.com>
2022-10-04 08:26:20 -0700

    multiplication

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - multiply two numbers and print results
+ * @argc: number of command line arguments
+ * @argv: array of strings representing the arguments
+ * Return: Success 0, 1 Failure
+ */
+
+int main(int argc, char *argv[])
+{
+	int result;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else 
+	{
+		result = argv[1] * argv[2];
+		printf("%d\n", result);
+	}
+	return (0);
+}
