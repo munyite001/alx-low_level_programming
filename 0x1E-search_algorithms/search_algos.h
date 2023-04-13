104351f99e04ab0a30b7af5ce002fa7bceda45fb
munyite001 <emunyite@gmail.com>
2023-04-13 23:55:07 +0300

    linear

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,7 +1,7 @@
-#ifndef search_algos_h
-#define search_algos_h
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
 #include <stddef.h>
 #include <stdio.h>
 int linear_search(int *array, size_t size, int value);
 
-#endif
+#endif /* SEARCH_ALGOS_H */
